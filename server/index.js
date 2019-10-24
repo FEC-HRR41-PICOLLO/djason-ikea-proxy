4acda64184abb1070c9932fcf7b030642f2755a0
ingdjason <djason_sylvaince@yahoo.fr>
2019-10-24 12:02:06 -0400

    render all services into my proxy server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = '3001';
+const PORT = '4000';
 // Enable All CORS Requests
 app.use(cors());
 app.use(express.static(`${__dirname}/../public/`));
